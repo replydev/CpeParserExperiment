99608fe347aa636fad45f4c6f6b6362b60b8c000
replydev <commoncargo@tutanota.com>
2025-09-16 20:17:53 +0200

    Create tests

--- src/test/java/lorenzo/cpe/CpeTest.java
+++ src/test/java/lorenzo/cpe/CpeTest.java
@@ -1,5 +1,28 @@
 package lorenzo.cpe;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class CpeTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "cpe:2.3:a:apache:http_server:2.4.53:*:*:*:*:*:*:*",
+            "cpe:2.3:a:apache:http_server:2.4.53:-:*:*:*:*:*:*",
+            "cpe:2.3:a:microsoft:iis:*:*:*:*:*:*:*:*",
+            "cpe:2.3:a:vendor:prod:-:*:*:*:*:*:*:*",
+            "cpe:2.3:a:vendor:prod:ver_*:*:*:*:*:*:*:*"
+    })
+    @DisplayName("Test CPE parsing")
+    void cpeAssert(final String cpe) {
+        // Arrange / Act
+        Cpe parsedCpe = Cpe.parse(cpe);
+
+        // Assert
+        assertNotNull(parsedCpe);
+    }
 
 }
